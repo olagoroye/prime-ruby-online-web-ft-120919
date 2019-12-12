0fea8b946d2e3663480d5e931e0ae3f3e4e41d1d
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-12 21:07:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(num)
+  x = 2 
+while x <= num
+if num == 1 || num <= 0 
+  return false
+elsif num == 2 
+    return true 
+else    
+  if num % x == 0  
+    return false 
+  end
+  x += 1 
+end
+end
+end
